e822e099d73978661776e6616106de557d096e1e
Roisaalfarizi <91373525+Roisaalfarizi@users.noreply.github.com>
2021-09-25 23:53:14 -1200

    tenor.java

--- coba git2/tenor.java
+++ coba git2/tenor.java
@@ -4,12 +4,6 @@ public class tenor{
 		System.out.println("Nama : Roisa");
 		System.out.println("NIM  : 6706210125");
 		System.out.println("Kelas: D3IF4501");
-		System.out.println("NIK  : 3215190309030004");
-		System.out.println("SMA  : As-Syifa Boarding School");
-		System.out.println("SMP  : Al-Hikmah cirebon");
-		System.out.println("TTL  : Telagasari Karawang");
-		System.out.println("TTL2 : 03-09-20003");
-		System.out.println("org yang disukai:Mona");
 		
 	}
 }
