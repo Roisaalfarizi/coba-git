0da6f63e6641725b856657c5b13d6ffc6c03c6ae
Roisaalfarizi <91373525+Roisaalfarizi@users.noreply.github.com>
2021-09-25 18:51:15 -1200

    tennor.java

---
+++ coba git2/tenor.java
@@ -0,0 +1,16 @@
+public class tenor{
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Roisa");
+		System.out.println("NIM  : 6706210125");
+		System.out.println("Kelas: D3IF4501");
+		System.out.println("NIK  : 3215190309030004");
+		System.out.println("SMA  : As-Syifa Boarding School");
+		System.out.println("SMP  : Al-Hikmah cirebon");
+		System.out.println("TTL  : Telagasari Karawang");
+		System.out.println("TTL2 : 03-09-20003");
+		System.out.println("org yang disukai:Mona");
+		
+	}
+}
+
